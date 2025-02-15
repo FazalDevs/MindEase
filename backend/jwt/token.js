63bb65a4fbfb178830859aef5deb6fb852f87835
FazalDevs <mohdfazalrm786@gmail.com>
2025-02-15 15:15:32 +0530

    final

--- backend/jwt/token.js
+++ backend/jwt/token.js
@@ -6,7 +6,7 @@ export const generateToken = async (id, res) => {
     });
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production,
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'none',
         path: '/',
     });
